959515886a59d7c64f060e104c29e994a00d903d
Tomé Luan <tomeluan05@gmail.com>
2022-12-03 13:36:21 -0300

    Organizando as pastas do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Header from './components/Header'
-import Body from './components/Body'
-import Footer from './components/Footer'
+import Header from './components/Header/Header'
+import Body from './components/Body/Body'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
